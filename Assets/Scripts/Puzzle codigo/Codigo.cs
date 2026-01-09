230a27204c836d1335a1689f57ed2eb9b619df49
Utsio <167820996+Utsio@users.noreply.github.com>
2026-01-09 22:40:22 +0100

    Prototipo puzzle codigo hecho

--- Assets/Scripts/Puzzle codigo/Codigo.cs
+++ Assets/Scripts/Puzzle codigo/Codigo.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
+using System;
 
 public class Codigo : MonoBehaviour
 {
     public int[] cod = new int[3];
     public int i = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static event Action<int[]> CodigoDig;
+
+    public void Enviar()
     {
-        
+        CodigoDig?.Invoke(cod);
     }
     public void Seleccionar(int b)
     {
--- Assets/Scripts/Puzzle codigo/Mensaje.cs
+++ Assets/Scripts/Puzzle codigo/Mensaje.cs
@@ -1,10 +1,18 @@
+using TMPro;
 using UnityEngine;
+
 
 public class Mensaje : MonoBehaviour
 {
+    public int[] sol;
+    public int ac;
+    public int ap;
+
+    public TMP_Text Pista;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Codigo.CodigoDig += Comprobar;
         
     }
 
@@ -12,5 +20,53 @@ public class Mensaje : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Comprobar(int[] cod) 
+    {
+        sol=new int[]{ 1, 2, 3 }
+        ;
+        ac = 0;
+        ap= 0;
+        for (int i = 0; i < sol.Length; i++) 
+        {
+            if (sol[i] == cod[i])
+            {
+                ac++;
+            }
+            else 
+            {
+                for (int j = 0; j < sol.Length; j++)
+                {
+                    if (sol[i] == cod[j] )
+                    {
+                        ap++;
+                        sol[i]=0;
+                    }
+                }
+            }
+        }
+
+        if (ac > 0) 
+        {
+            if (ap > 0)
+            {
+                Pista.text = "Has tenido " + ac + " aciertos y " + ap + " aproximaciones";
+            }
+            else 
+            {
+                Pista.text = "Has tenido " + ac + " aciertos";
+            }
+        }
+        else
+        {
+            if (ap > 0)
+            {
+                Pista.text = "Has tenido " + ap + " aproximaciones";
+            }
+            else
+            {
+                Pista.text = "Nada esta bien";
+            }
+        }
     }
 }
