b7f702566c752ecb8ed3fff856f551e04c4b80c6
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2026-01-17 18:12:44 +0100

    collider del balcon

--- Assets/Scripts/Interactive Objects/LadderUnlock.cs
+++ Assets/Scripts/Interactive Objects/LadderUnlock.cs
@@ -6,7 +6,7 @@ public class LadderUnlock : MonoBehaviour
     //cuando se hace click en la parte de abajo de la escalera
     void OnMouseDown()
     {
-        //se ha tenido que abrir el coche primero
+        //se ha tenido que abrir el coche primero para desbloquear este puzzle
         if (GameStateSingleton.Instance.carUnlocked)
         {
             SceneManager.LoadScene("PuzzleCodigo");
