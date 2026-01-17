22da90162ae036d76fd0feaa290872bbfe7e3ab9
Sara <samepae01@gmail.com>
2026-01-17 18:46:37 +0100

    Gemitas la segunda parte

--- Assets/Scripts/GemsFlyweight/GemsCounter.cs
+++ Assets/Scripts/GemsFlyweight/GemsCounter.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using TMPro;
 
 public class GemsCounter : MonoBehaviour
 {
     public static GemsCounter Instance;
-    public int totalGems;
+    public int totalGems=0;
+    public TMP_Text counterText;
 
     void Awake()
     {
@@ -12,11 +14,22 @@ public class GemsCounter : MonoBehaviour
         else
             Destroy(gameObject);
     }
-
+    void Start()
+    {
+        UpdateUI();
+    }
     public void AddGem(int value)
     {
         totalGems += value;
-        Debug.Log("Gemas encontradas: " + totalGems);
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (counterText != null)
+        {
+            counterText.text = "Gemas: " + totalGems;
+        }
     }
 }
 
