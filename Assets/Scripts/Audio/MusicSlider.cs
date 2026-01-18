d8810d0cb08d8ff110ebe5df92be5f18523832fd
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2026-01-18 14:30:13 +0100

    slider bonito

--- Assets/Scripts/Audio/MusicSlider.cs
+++ Assets/Scripts/Audio/MusicSlider.cs
@@ -1,26 +1,12 @@
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
 
 public class MusicSlider : MonoBehaviour
 {
     public AudioMixer mixer;
-    //public Slider slider;
 
     public void SliderControl (float slider)
     {
         mixer.SetFloat("GlobalVolume", Mathf.Log10(slider) * 20);
     }
-    /*void Start()
-    {
-        float value;
-        mixer.GetFloat("GlobalVolume", out value);
-        slider.value = value;
-    }
-
-    public void SetVolume(float volume)
-    {
-        mixer.SetFloat("GlobalVolume", volume);
-    }*/
 }
